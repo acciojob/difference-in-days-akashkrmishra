d3f5f9b6ab15e6fc4e7bc1271b71b6a21104d44a
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-01-14 13:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	let n1=parseInt(date1.slice(-2,date1.length));
+	let n2=parseInt(date2.slice(-2,date1.length));
+	return n2-n1;
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+// const dateOne = prompt("Enter Start Date.");
+// const dateTwo = prompt("Enter End Date.");
+// alert(dateDiffInDays(dateOne, dateTwo));
